df777ebe8ca347a312e74b5fc04760534123c926
SameeraThilakaratne <sameera.anuradha@globalwavenet.com>
2020-04-01 15:00:55 +0530

    First commit

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      //require('karma-chrome-launcher'),/*comment out this line to disable the karma-chrome-launcher*/
+      require('karma-phantomjs-launcher'),  /* add this line to disable the karma-phantomjs-launcher*/
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,8 +26,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    //browsers: ['Chrome'],
+    browsers: ['PhantomJS'], /*remove chrome and replace it with PhantomJS */
+    singleRun: true,  /*make it true to run test suits only one time*/
+    //singleRun: false,
     restartOnFileChange: true
   });
 };
